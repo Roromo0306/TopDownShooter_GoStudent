b3b767207ff2ff2b3aa0d13515984591e61db4df
Roromo0306 <pownyrainbow03@gmail.com>
2025-11-02 17:58:03 +0100

    Nuevo menu principal

---
+++ TopDownShooter_GoStudent/Assets/Scripts/MainMenuController.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuController : MonoBehaviour
+{
+   public void Jugar()
+    {
+        SceneManager.LoadScene("Game");
+    }
+
+
+    public void Salir()
+    {
+        Application.Quit();
+    }
+}
